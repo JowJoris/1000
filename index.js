fee59553047422b8a0198617c3cd3e6c184b851b
Joris Huinink <joris.huinink@gmail.com>
2019-06-18 19:27:33 +0200

    Working version

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-function berekenScore(){
-let vijf = parseInt(document.getElementById('vijf').value)*5;
-let tien = parseInt(document.getElementById('tien').value)*10;
-let vijftien = parseInt(document.getElementById('vijftien').value)*15;
-let honderd = parseInt(document.getElementById('honderd').value)*100;
-let berekend = vijf + tien + vijftien + honderd;
-document.getElementById('berekend').innerHTML = berekend;
-updateTotaal(berekend);
+function berekenScore() {
+  let getallen = [
+    parseInt(document.getElementById('vijf').value),
+    parseInt(document.getElementById('tien').value),
+    parseInt(document.getElementById('azen').value),
+    parseInt(document.getElementById('jokers').value),
+    parseInt(document.getElementById('schoppenVrouw').value)
+  ]
+
+  let vijf =  getallen[0]* 5;
+  let tien =  getallen[1]* 10;
+  let azen = getallen[2] * 15;
+  let jokers = getallen[3] * 25;
+  let schoppenVrouw =  getallen[4]* 100;
+
+  let berekend = vijf + tien + azen + jokers + schoppenVrouw;
+  document.getElementById('berekend').innerHTML = berekend;
+  updateTotaal(berekend);
 }
 
-function setTotaal(){
-  document.getElementById('totaal').innerHTML = document.getElementById('totaal').value
+function setTotaal() {
+  document.getElementById('totaal').innerHTML = document.getElementById('input').value;
 }
 
-function updateTotaal(berekend){
-  let totaal = parseInt(document.getElementById('totaal').value);
-  document.getElementById('totaal').innerHTML = totaal + berekend;
+function updateTotaal(berekend) {
+  document.getElementById('totaal').innerHTML = Number(document.getElementById('totaal').innerHTML) + berekend;
 }
