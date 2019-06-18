04b92fafd9fa24ab4c96959791cf4b28bb72ca5d
Joris Huinink <joris.huinink@gmail.com>
2019-06-18 20:25:30 +0200

    Fixed NaN bug

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 function berekenScore() {
   let getallen = [
-    parseInt(document.getElementById('vijf').value),
-    parseInt(document.getElementById('tien').value),
-    parseInt(document.getElementById('azen').value),
-    parseInt(document.getElementById('jokers').value),
-    parseInt(document.getElementById('schoppenVrouw').value)
+    checkWaarde(parseInt(document.getElementById('vijf').value)),
+    checkWaarde(parseInt(document.getElementById('tien').value)),
+    checkWaarde(parseInt(document.getElementById('azen').value)),
+    checkWaarde(parseInt(document.getElementById('jokers').value)),
+    checkWaarde(parseInt(document.getElementById('schoppenVrouw').value))
   ]
-
-  let vijf =  getallen[0]* 5;
-  let tien =  getallen[1]* 10;
-  let azen = getallen[2] * 15;
-  let jokers = getallen[3] * 25;
-  let schoppenVrouw =  getallen[4]* 100;
+  let vijf            = getallen[0] * 5;
+  let tien            = getallen[1] * 10;
+  let azen            = getallen[2] * 15;
+  let jokers          = getallen[3] * 25;
+  let schoppenVrouw   = getallen[4] * 100;
 
   let berekend = vijf + tien + azen + jokers + schoppenVrouw;
   document.getElementById('berekend').innerHTML = berekend;
   updateTotaal(berekend);
+}
+
+function checkWaarde(waarde){
+  if(waarde !== waarde){
+    return 0;
+  }
+  return waarde;
 }
 
 function setTotaal() {
